f23f4f81ebdd18ee7fba8202149c1870068fff5e
Sean Collins <sgc29@zips.uakron.edu>
2017-10-30 01:06:45 -0400

    Add message to testing_main.cpp.

--- cpp-testing/testing/testing_main.cpp
+++ cpp-testing/testing/testing_main.cpp
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
+#include <iostream>
 
 // Include your tests here:
 
 int main (int argc, char** argv)
 {
+  std::cout << "Main function for running tests." << std::endl;
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
